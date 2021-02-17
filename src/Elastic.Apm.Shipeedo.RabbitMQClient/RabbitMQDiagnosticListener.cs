3e40e0e966ba80d644b327cda0661ae539529ae3
stousmand <shirhan.apache@gmail.com>
2021-02-17 19:21:34 +0530

    Commits initial RabbitMQ Client Project
    Idea was taken from the GrpcClient project that was already in the repository.

---
+++ src/Elastic.Apm.Shipeedo.RabbitMQClient/RabbitMQDiagnosticListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Elastic.Apm.DiagnosticSource;
+
+namespace Elastic.Apm.RabbitMQClient
+{
+	public class RabbitMQDiagnosticListener : IDiagnosticListener
+	{
+		private readonly IApmAgent _agent;
+
+		public RabbitMQDiagnosticListener(IApmAgent apmAgent) => _agent = apmAgent;
+		public string Name => "RabbitMQ.Net.Client";
+
+		public void OnCompleted() => throw new NotImplementedException();
+		public void OnError(Exception error) => throw new NotImplementedException();
+		public void OnNext(KeyValuePair<string, object> value) => throw new NotImplementedException();
+	}
+}
